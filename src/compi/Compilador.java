f28899bb27b2a52636ed91654400973c8b151737
amonterol <abmontero@yahoo.com>
2024-09-22 14:46:04 -0600

    4 Identifica cadenas de caracteres alfanumericos y con guion bajo

--- src/compi/Compilador.java
+++ src/compi/Compilador.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package compilador;
+package compi;
 
 import java.io.IOException;
 import javax.swing.JOptionPane;
